3034b70a2980b0636ee2c0c41a165d9a2bd3c30d
tonymushah <95529016+tonymushah@users.noreply.github.com>
2023-02-08 17:09:19 -0500

    template update

--- application/views/frontoffice/templates.php
+++ application/views/frontoffice/templates.php
@@ -1,6 +1,5 @@
 <?php
 	$this->load->view("frontoffice/templates/sidebar", array("title" => $title, "current_user" => $current_user));
 	$this->load->view($contents, $data);
-	var_dump($data);
 	$this->load->view("frontoffice/templates/footer");
 ?>
